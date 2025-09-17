d1be5ba5e6d01991f4a0e05aed601ff63c5b3c9a
Duke Norsworthy <duke.norsworthy@gmail.com>
2025-09-16 23:31:53 -0400

    job board added

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Navbar } from './components/navbar/navbar';
+import { Jobboard } from './components/jobboard/jobboard';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Navbar, Jobboard],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
---
+++ src/app/components/jobboard/jobboard.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-jobboard',
+  standalone: true,
+  templateUrl: './jobboard.html',
+  styleUrls: ['./jobboard.scss']
+})
+export class Jobboard {}
